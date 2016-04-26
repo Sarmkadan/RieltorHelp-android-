9ee7de522f3d93e8ef255f2ed3ac4e548f705e69
CavInc <cavinc20@gmail.com>
2016-04-26 20:57:21 +0700

    Добавлена активити для синхронизации

---
+++ RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/SyncActivity.java
@@ -0,0 +1,19 @@
+package com.example.sarmkadan.rieltorhelper;
+/**
+ * @author Kotov Alexandr
+ */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+public class SyncActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sync);
+    }
+
+
+}
