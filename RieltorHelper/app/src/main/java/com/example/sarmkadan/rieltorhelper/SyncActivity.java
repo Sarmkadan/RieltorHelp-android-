09535c712f0cef94169a1784b99d05c80920f8ed
CavInc <cavinc20@gmail.com>
2016-04-26 21:05:37 +0700

    Работа над активити SyncActivity

--- RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/SyncActivity.java
+++ RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/SyncActivity.java
@@ -4,16 +4,21 @@ package com.example.sarmkadan.rieltorhelper;
  */
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.widget.Button;
 
 
 public class SyncActivity extends AppCompatActivity {
+    private Button btnSync;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync);
-    }
 
+        // наша  кнопка
+        btnSync = (Button) findViewById(R.id.btnSync);
+
+
+    }
 
 }
