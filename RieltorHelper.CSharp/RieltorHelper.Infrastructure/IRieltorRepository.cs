96ed0f4647193957f41fdeb85187dd13898facbd
koskapan <koskapan@gmail.com>
2016-04-27 23:23:47 +0300

    IRieltorRepository interface cast for UOW pattern

--- RieltorHelper.CSharp/RieltorHelper.Infrastructure/IRieltorRepository.cs
+++ RieltorHelper.CSharp/RieltorHelper.Infrastructure/IRieltorRepository.cs
@@ -5,13 +5,13 @@ using System.Text;
 
 namespace RieltorHelper.Infrastructure
 {
-    public interface IRieltorRepository
+    public interface IRieltorRepository<T> where T: class
     {
         int GetTablesCount();
-        IEnumerable<IDataTable> GetTables(int offset, int count);
-        IDataTable GetTable(int id);
-        void CreateTable(IDataTable table);
-        void EditTable(int id, IDataTable table);
+        IEnumerable<T> GetTables(int offset, int count);
+        T GetTable(int id);
+        void CreateTable(T table);
+        void EditTable(int id, T table);
         void DeleteTable(int id);
     }
 }
