590c05a14c60f831b96dc5ad596baf78ef3f3544
cav <cavinc20@gmail.com>
2016-04-27 15:26:40 +0700

    Сделан запрос данных по HTTP

--- RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/utils/HttpRequest.java
+++ RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/utils/HttpRequest.java
@@ -2,13 +2,12 @@ package com.example.sarmkadan.rieltorhelper.utils;
 
 import android.util.Log;
 
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
 
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+
 
 /**
  * Created by cav on 26.04.16.
@@ -19,12 +18,15 @@ public class HttpRequest {
 
     public static String getRequest(String urlRequest){
         try{
-            URL url = new URL(urlRequest);
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(new InputSource(url.openStream()));
-            doc.getDocumentElement().normalize();
-
+            URLConnection conn = new URL(urlRequest).openConnection();
+            InputStreamReader rd = new InputStreamReader(conn.getInputStream());
+            StringBuilder allpage = new StringBuilder();
+            int n = 0;
+            char[] buffer = new char[40000];
+            while ((n=rd.read(buffer,0,buffer.length))!=-1){
+                allpage.append(buffer,0,n);
+            }
+            rd.close();
         }catch (Exception e){
             Log.d(LOG_TAG,e.getMessage());
             return "Error: "+e.getLocalizedMessage();
