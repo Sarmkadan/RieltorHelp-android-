abec54168c9a5c8ec7ad8979360164b21da39012
CavInc <cavinc20@gmail.com>
2016-05-04 17:04:08 +0700

    Эксперименты с Rest

--- RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/utils/RestWorker.java
+++ RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/utils/RestWorker.java
@@ -3,14 +3,23 @@ package com.example.sarmkadan.rieltorhelper.utils;
 /**
  * Created by Kotov Alexandr on 04.05.16.
  */
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 public class RestWorker {
     /*
       Рыба для обработки Rest
      */
+    HttpClient httpclient;
+
     public void RestWorker(){
+        httpclient = new DefaultHttpClient();
 
     }
     public void GetRest(){
+        HttpGet httpget = new HttpGet("http://kempir.com/testrest");
 
     }
     public void PostRest(){
