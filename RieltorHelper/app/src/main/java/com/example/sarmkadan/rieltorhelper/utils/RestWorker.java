aa69bcffe902907a0a6afcd46a547fbb122e0c35
CavInc <cavinc20@gmail.com>
2016-05-04 15:20:17 +0700

    Добавлена рыба для Rest запросов

---
+++ RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/utils/RestWorker.java
@@ -0,0 +1,25 @@
+package com.example.sarmkadan.rieltorhelper.utils;
+
+/**
+ * Created by Kotov Alexandr on 04.05.16.
+ */
+public class RestWorker {
+    /*
+      Рыба для обработки Rest
+     */
+    public void RestWorker(){
+
+    }
+    public void GetRest(){
+
+    }
+    public void PostRest(){
+
+    }
+    public void PutRest(){
+
+    }
+    public void DeleteRest(){
+
+    }
+}
