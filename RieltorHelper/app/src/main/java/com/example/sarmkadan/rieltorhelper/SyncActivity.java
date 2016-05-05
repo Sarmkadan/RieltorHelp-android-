e346e2c8c609aa366fd5e508b4f29fca5466b4fd
CavInc <cavinc20@gmail.com>
2016-05-05 16:10:20 +0700

    Эксперименты с Rest получение данных

--- RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/SyncActivity.java
+++ RieltorHelper/app/src/main/java/com/example/sarmkadan/rieltorhelper/SyncActivity.java
@@ -10,11 +10,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.sarmkadan.rieltorhelper.utils.HttpRequest;
+import com.example.sarmkadan.rieltorhelper.utils.RestWorker;
 
 
 public class SyncActivity extends AppCompatActivity {
     private Button btnSync;
     private TextView outView;
+
+    private TextView respTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,13 @@ public class SyncActivity extends AppCompatActivity {
         // текстовое поле
         outView = (TextView) findViewById(R.id.synTextView);
         outView.setText("Проверка работы кнопки");
+
+        // ------------------------------------------
+        // проверяем рабту рест запроса
+        respTV = (TextView) findViewById(R.id.responseTV);
+
+        Button btnGetTest=(Button) findViewById(R.id.btnTestGetButton);
+        // --------------------------------------------
 
         // Устанавливаем разрешения
         if (android.os.Build.VERSION.SDK_INT > 9)
@@ -41,6 +51,14 @@ public class SyncActivity extends AppCompatActivity {
                 String s= HttpRequest.getRequest("http://mydefence.h1n.ru/script/?op=set&userid=2");
                 outView.setText(s);
 
+            }
+        });
+
+        btnGetTest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                RestWorker rwt=new RestWorker();
+                respTV.setText(rwt.GetRest());
             }
         });
 
